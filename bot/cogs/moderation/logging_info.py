352377c0d3a1b8be1e807f17a77ab350a1c7f94e
Rashaad Akbar <rashaadzma@gmail.com>
2021-07-17 17:11:08 +0530

    Minor changes

--- bot/cogs/moderation/logging_info.py
+++ bot/cogs/moderation/logging_info.py
@@ -1,4 +1,4 @@
-import discord
+import discord, asyncpg
 from discord import Member, Embed
 import typing as t
 
@@ -8,33 +8,69 @@ from discord.ext.commands import Context, Cog, command
 
 from bot.main import Bot
 from bot.utils.paginator import CustomPaginator
-from bot.utils.helpful import build_error_embed
+from bot.utils.helpful import build_error_embed, build_success_embed
 from bot.utils.checks import is_staff, is_moderator, is_admin
-from bot.constants import Colours
+from bot.constants import Colours, RushGuild
 
 
 class LoggingInformation(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.db = self.bot.db
         self.members = self.bot.db.members
 
     @Cog.listener()
     async def on_message(self, message: discord.Message):
-        if not message.author.bot and message.guild:
+        if not message.guild:
+            return
+        
+        if message.guild.id != RushGuild.id:
+            return
+
+        if not message.author.bot:
             member_obj = await self.members.objects.get(message.author.id)
             if member_obj:
-                member_obj.message_count += 1
-                await member_obj.update()
-
                 await self.bot.db.messages(
                     id=message.id,
                     author=member_obj,
                     content=message.content or f"{message.author} sent a image",
+                    channel_id=message.channel.id,
                     sent_at=message.created_at,
                 ).save()
 
             elif not member_obj:
-                member = message.author
+                try:
+                    member = message.author
+                    await self.members(
+                        id=member.id,
+                        name=member.name,
+                        discriminator=member.discriminator,
+                        in_guild=True,
+                        joined_at=member.joined_at,
+                    ).save()
+                except asyncpg.exceptions.UniqueViolationError:
+                    pass
+
+    @Cog.listener()
+    async def on_message_edit(self, before, after):
+        if not after.guild:
+            return
+        
+        if after.content != before.content:
+            return
+
+        if after.guild.id != RushGuild.id:
+            return
+
+        message_obj = await self.db.messages.objects.get(after.id)
+        if message_obj:
+            message_obj.content = after.content
+            await message_obj.update()
+
+    @Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        if not member.bot:
+            try:
                 await self.members(
                     id=member.id,
                     name=member.name,
@@ -42,17 +78,10 @@ class LoggingInformation(commands.Cog):
                     in_guild=True,
                     joined_at=member.joined_at,
                 ).save()
-
-    @Cog.listener()
-    async def on_member_join(self, member: discord.Member):
-        if not member.bot:
-            await self.members(
-                id=member.id,
-                name=member.name,
-                discriminator=member.discriminator,
-                in_guild=True,
-                joined_at=member.joined_at,
-            ).save()
+            except asyncpg.exceptions.UniqueViolationError:
+                member_obj = await self.members.objects.get(member.id)
+                member_obj.in_guild = True
+                await member_obj.update()
 
     @Cog.listener()
     async def on_member_remove(self, member: discord.Member):
@@ -70,7 +99,9 @@ class LoggingInformation(commands.Cog):
             member_in_db.discriminator = after.discriminator
             await member_in_db.update()
 
-    @commands.group(name="database", aliases=["db", "databases"], invoke_without_command=True)
+    @commands.group(
+        name="database", aliases=["db", "databases"], invoke_without_command=True
+    )
     async def database_command(self, ctx: Context):
         """The main group command for getting information from the database"""
         await ctx.send_help(ctx.command)
@@ -81,12 +112,15 @@ class LoggingInformation(commands.Cog):
         """Sends information about all members in the database"""
         all_members = list()
         for db_member in await self.members.objects.all():
-            member_info = f"""{db_member} ID: {db_member.id} Messages Sent: {db_member.message_count} 
-                               Joined At: {db_member.joined_at} Total infractions: {db_member.total_infractions}\n"""
+            messages_sent = await self.bot.db.messages.objects.filter(
+                author=db_member.id
+            )
+            member_info = f"""**{db_member} (ID: {db_member.id})**\nMessages Sent: {messages_sent.count()} 
+                               Joined At: {db_member.joined_at}\nTotal infractions: {db_member.total_infractions}\n"""
             all_members.append(member_info)
-        embed = Embed(title="All members in the database", colour=Colours.blue)
+        embed = Embed(title="All members in the database", colour=Colours.light_blue)
 
-        await CustomPaginator(all_members, embed).paginate(ctx)
+        await CustomPaginator(all_members, embed, items_per_page=5).paginate(ctx)
 
     @database_command.command(name="member-info", aliases=["mi"])
     @is_moderator()
@@ -101,17 +135,83 @@ class LoggingInformation(commands.Cog):
             )
         else:
             embed = discord.Embed(title=f"Information of {member.name}")
+            messages_sent = await self.db.messages.objects.filter(author=member.id)
             description = str()
             description += f"Username: {str(member_obj)}\n"
             description += f"Id: {member_obj.id}\n"
             description += f"In Guild: {member_obj.in_guild}\n"
-            description += f"Total Messages Sent: {member_obj.message_count}\n"
-            description += f"Total Infractions Received: {member_obj.total_infractions}\n"
+            description += f"Total Messages Sent: {messages_sent.count()}\n"
+            description += (
+                f"Total Infractions Received: {member_obj.total_infractions}\n"
+            )
             description += (
                 f"Joined at: {member_obj.joined_at.strftime('%d/%m/%Y %H:%M:%S')}\n"
             )
             embed.description = description
             await ctx.send(embed=embed)
+
+    @database_command.command(name="sync-members-db", aliases=["smd"])
+    @commands.is_owner()
+    async def sync_members_database(self, ctx: Context):
+        for member in ctx.guild.members:
+            if not member.bot:
+                try:
+                    await self.members(
+                        id=member.id,
+                        name=member.name,
+                        discriminator=member.discriminator,
+                        in_guild=True,
+                        joined_at=member.joined_at,
+                    ).save()
+                except asyncpg.exceptions.UniqueViolationError:
+                    pass
+        await ctx.send(
+            embed=build_success_embed(
+                "Synced Members Database with current server members"
+            )
+        )
+
+    @database_command.command(name="messages")
+    @is_moderator()
+    async def database_messages_command(
+        self, ctx: Context, message_count: t.Optional[int] = None
+    ):
+        all_messages = await self.db.messages.objects.all()
+        try:
+            all_messages.raw[:message_count]
+        except IndexError:
+            await ctx.send(
+                embed=build_error_embed(f"Index {message_count} doesn't exist")
+            )
+        to_send = list()
+        for db_message in all_messages.raw[:message_count]:
+            formatted = f"""Message Id: {db_message.id}\nAuthor: <@!{db_message.author}>
+                            Content:\n{db_message.content}
+                            Channel id: {db_message.channel_id}\nDate sent: {db_message.sent_at}\n"""
+            to_send.append(formatted)
+        await CustomPaginator(to_send).paginate(ctx)
+
+    @database_command.command(
+        name="messages-from", aliases=["msg-from", "msgs-from", "m-from"]
+    )
+    @is_moderator()
+    async def database_messages_from_command(
+        self, ctx: Context, from_member: Member, message_count: t.Optional[int] = None
+    ):
+        all_messages = await self.db.messages.objects.filter(author=from_member.id)
+        try:
+            all_messages.raw[:message_count]
+        except IndexError:
+            await ctx.send(
+                embed=build_error_embed(f"Index {message_count} doesn't exist")
+            )
+        to_send = list()
+        for db_message in all_messages.raw[:message_count]:
+            formatted = f"""Message Id: {db_message.id}\nAuthor: <@!{db_message.author}>
+                            Content:\n{db_message.content}
+                            Channel id: {db_message.channel_id}\nDate sent: {db_message.sent_at}\n"""
+            to_send.append(formatted)
+        await CustomPaginator(to_send).paginate(ctx)
 
 
 def setup(bot: Bot):
